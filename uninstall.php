dfff7c09d42749f813bbfe4254c7861623fc7fd4
Ste <hello+github@stewright.me>
2026-04-06 12:14:58 +0100

    More fixes

--- uninstall.php
+++ uninstall.php
@@ -3,9 +3,23 @@ if (! defined('WP_UNINSTALL_PLUGIN')) {
 	exit;
 }
 
-// Delete all saved widget configurations.
-delete_option('tttw_widgets');
+function tttw_uninstall() {
+	delete_option( 'tttw_widgets' );
 
-// Delete all cached transients created by the plugin.
-global $wpdb;
-$wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE '_transient_tttw_cache_%' OR option_name LIKE '_transient_timeout_tttw_cache_%'");
+	global $wpdb;
+	$transient_names = wp_cache_get( 'tttw_uninstall_transients' );
+	if ( false === $transient_names ) {
+		$transient_names = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT option_name FROM {$wpdb->options} WHERE option_name LIKE %s",
+				$wpdb->esc_like( '_transient_tttw_cache_' ) . '%'
+			)
+		);
+		wp_cache_set( 'tttw_uninstall_transients', $transient_names );
+	}
+	foreach ( $transient_names as $option_name ) {
+		delete_transient( substr( $option_name, strlen( '_transient_' ) ) );
+	}
+}
+
+tttw_uninstall();
